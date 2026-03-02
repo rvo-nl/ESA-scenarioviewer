8ffa19775632fdf4768f7e74d15f4c99c002288d
tijs <85936126+entranos@users.noreply.github.com>
2026-03-02 12:41:53 +0100

    bugfix

--- PBL/js/loadData.js
+++ PBL/js/loadData.js
@@ -80,7 +80,7 @@ async function decryptData(encryptedData, key, iv) {
 async function decryptZipFile(passphrase) {
   try {
     // Fetch the encrypted file
-    const response = await fetch('public/ds09022026pbl.enc.json');
+    const response = await fetch('public/ds02032026pbl.enc.json');
     if (!response.ok) {
       throw new Error(`Failed to fetch encrypted file: ${response.status}`);
     }
